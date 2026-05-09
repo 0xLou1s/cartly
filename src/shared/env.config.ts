27dea24984bf174d170eb79371c43bdcb4148ba0
0xLou1s <lou1svuong.dev@gmail.com>
2026-05-09 23:09:01 +0700

    feat: add OTP module (#9)

--- src/shared/env.config.ts
+++ src/shared/env.config.ts
@@ -19,6 +19,7 @@ const configSchema = z.object({
   REFRESH_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_EXPIRES_IN: z.string() as z.ZodType<JwtSignOptions['expiresIn']>,
   SECRET_API_KEY: z.string(),
+  OTP_EXPIRES_IN: z.string(),
   // admin
   ADMIN_EMAIL: z.string(),
   ADMIN_PASSWORD: z.string(),
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { Global, Module } from '@nestjs/common'
-import { PrismaService } from 'src/shared/services/prisma.service'
-import { HashingService } from './services/hashing.service'
-import { TokenService } from './services/token.service'
+import { APP_GUARD } from '@nestjs/core'
 import { JwtModule } from '@nestjs/jwt'
 import { AccessTokenGuard } from 'src/shared/guards/access-token.guard'
 import { APIKeyGuard } from 'src/shared/guards/api-key.guard'
-import { APP_GUARD } from '@nestjs/core'
 import { AuthenticationGuard } from 'src/shared/guards/authentication.guard'
+import { PrismaService } from 'src/shared/services/prisma.service'
+import { SharedUserRepository } from './repositories/shared-user.repo'
+import { HashingService } from './services/hashing.service'
+import { TokenService } from './services/token.service'
 
-const sharedServices = [PrismaService, HashingService, TokenService]
+const sharedServices = [PrismaService, HashingService, TokenService, SharedUserRepository]
 
 @Global()
 @Module({
