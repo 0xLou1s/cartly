299a892b03cc8711a02c04918dc3aa7d810e3966
lou1s <lou1svuong.dev@gmail.com>
2026-05-09 08:01:38 +0700

    feat: add swagger with nestjs-zod integration
    - Install @nestjs/swagger
    - Bootstrap SwaggerModule at /api-docs with bearer auth
    - Use cleanupOpenApiDoc from nestjs-zod so zod DTOs render correctly
    - Tag the auth controller with @ApiTags

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { cleanupOpenApiDoc } from 'nestjs-zod'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Cartly API')
+    .setDescription('Cartly API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const document = cleanupOpenApiDoc(SwaggerModule.createDocument(app, config))
+  SwaggerModule.setup('api-docs', app, document)
+
   await app.listen(process.env.PORT ?? 4000)
 }
 bootstrap()
